d53644a2e6c9ddb4f01bc6161f28ac51cd801d5a
xerinay <xerinay85@gmail.com>
2020-04-09 21:17:01 -0400

    q

---
+++ src/test/java/branck3.java
@@ -0,0 +1,14 @@
+public class branck3 {
+    public void testOne(){
+        System.out.println("hello world!");
+        String expected="a";
+        String actual ="b";
+        if(expected.equals(actual)) {
+
+        }else {
+            System.out.println("fail");
+        }
+
+        //this is how u test in TestNG
+    }
+}
